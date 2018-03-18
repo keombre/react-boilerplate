e8828bd471363c400b61997ee7abd877b06561bc
Keombre <keombre8@gmail.com>
2018-03-18 21:48:25 +0100

    Jest added

---
+++ __tests__/pass.ts
@@ -0,0 +1,5 @@
+describe('root', () => {
+    it('should pass', () => {
+      expect(1 + 2).toBe(3)
+    })
+})
---
+++ src/fail.test.ts
@@ -0,0 +1,5 @@
+describe('inside', () => {
+    it('should fail', () => {
+      expect(1 + 2).toBe(4)
+    })
+})
